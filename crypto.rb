9a18f8791f595a83f69c98736148c452bc079131
oseus <36455199+oseus@users.noreply.github.com>
2018-04-13 00:19:19 +0200

    Update crypto.rb

--- crypto.rb
+++ crypto.rb
@@ -7,9 +7,10 @@ def money
 
    cur_rate = page.css('a.price').text
    cur_name = page.css('a.currency-name-container').text
-   bit = Hash.new { |cur_name,cur_rate| cur_name[cur_rate] }
-     puts bit
-   end
+   bit = Hash.new (0)
+   bit = {cur_name => cur_rate}
+   bit.each {|nom, bourse| puts "#{nom}: #{bourse}"}
+
 end
 
 money
